fec78e6188b190d5a924cd3749ca42b25e4bf5e4
Tanishq <80031574+tanishqsisodiya@users.noreply.github.com>
2023-07-31 23:46:10 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> list = new ArrayList<>();
+        int arr[] = new int[V+1];
+        Queue<Integer> q = new LinkedList<>();
+        arr[0] = 1;
+        q.add(0);
+        
+        while(!q.isEmpty()){
+            int x = q.peek();
+            q.remove();
+            list.add(x);
+            
+            for(int i=0; i<adj.get(x).size(); i++){
+                if(arr[adj.get(x).get(i)]==0){
+                    arr[adj.get(x).get(i)]=1;
+                    q.add(adj.get(x).get(i));
+                }
+            }
+        }
+        
+        return list;
+    }
+}
